bed417e513fdaab77432f257a4a69cfe7e6a3427
hank Liu <hankliu@hankdeMacBook-Air.local>
2024-12-16 18:01:52 +0800

    [fix]修改表單欄位

--- src/RegistrationPage.jsx
+++ src/RegistrationPage.jsx
@@ -1,31 +1,57 @@
 import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 
 function RegistrationPage() {
     const [formData, setFormData] = useState({
-        userName: '',
+        account: '',
         email: '',
+        promotionCode:'',
         password: '',
-        phoneNumber:''
+        confirmPassword:'',
+        phoneNumber:'',
+        firstName:'',
+        secondName:'',
+        cardNo:'',
+        mpin:''
+    });
+    const [resp, setResponse] = useState({
+        code:'',
+        error_msg:'',
+        result: {
+            regist:''
+        }
     });
 
     const handleChange = (e) => {
         const { name, value } = e.target;
+        console.log("handleChange name: ", name, ' + value: ', value)
         setFormData({
             ...formData,
             [name]: value
         });
     };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    const handleSubmit = async(event) => {
+        event.preventDefault();
         console.log('Form submitted:', formData);
-        alert('onClick')
         // Add form submission logic here
+        axios.post(
+            'https://run.mocky.io/v3/bc0997ca-458a-478c-b9f5-efbaa306ff8d', 
+            formData)
+        .then(response => {
+            setResponse(response.data)
+            // setResponse(response.data);
+            
+            var code = resp.result.regist
+            console.log('resp code: ', code)
+            // alert('response code: ', code);
+        })
+        .catch(error => {
+            alert('error: ', error);
+        });
     };
-
-
 
     return (
         <div className="registration-container">
@@ -39,45 +65,100 @@ function RegistrationForm({ formData, handleChange, handleSubmit }) {
     return (
         <div className='form-container'>
             <form onSubmit={handleSubmit}>
-                <FormInput
-                    label="UserName"
-                    type="text"
-                    id="user_name"
-                    name="user_name"
-                    value={formData.username}
-                    handleChange={handleChange}
-                    required
-                />
-                <FormInput
-                    label="Email"
-                    type="email"
-                    id="email"
-                    name="email"
-                    value={formData.email}
-                    handleChange={handleChange}
-                    required
-                />
-                <FormInput
-                    label="Password"
-                    type="password"
-                    id="password"
-                    name="password"
-                    value={formData.password}
-                    handleChange={handleChange}
-                    required
-                />
-                <FormInput
-                    label="phoneNumber"
-                    type="phone"
-                    id="phoneNumber"
-                    name="phoneNumber"
-                    value={formData.phoneNumber}
-                    handleChange={handleChange}
-                    required
-                />
+                <label className='label-title'>User Information</label>
+                <div className='form-type'>
+                    <FormInput
+                        label="*Account"
+                        type="text"
+                        id="account"
+                        name="account"
+                        value={formData.account}
+                        handleChange={handleChange}
+                        required
+                    />
+                    <FormInput
+                        label="*Email"
+                        type="email"
+                        id="email"
+                        name="email"
+                        value={formData.email}
+                        handleChange={handleChange}
+                        required
+                    />
+                    <FormInput
+                        label="*Promotion Code"
+                        type="text"
+                        id="promotionCode"
+                        name="promotionCode"
+                        value={formData.promotionCode}
+                        handleChange={handleChange}
+                        required
+                    />
+                    <FormInput
+                        label="*Password"
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={formData.password}
+                        handleChange={handleChange}
+                        required
+                    />
+                    <FormInput
+                        label="*Confirm Password"
+                        type="password"
+                        id="confirmPassword"
+                        name="confirmPassword"
+                        value={formData.confirmPassword}
+                        handleChange={handleChange}
+                        required
+                    />
+                    <FormInput
+                        label="*phone"
+                        type="number"
+                        id="phoneNumber"
+                        name="phoneNumber"
+                        value={formData.phoneNumber}
+                        handleChange={handleChange}
+                        required
+                    />
+                    <FormInput
+                        label="First Name"
+                        type="text"
+                        id="firstName"
+                        name="firstName"
+                        value={formData.firstName}
+                        handleChange={handleChange}
+                    />
+                    <FormInput
+                        label="Second Name"
+                        type="text"
+                        id="secondName"
+                        name="secondName"
+                        value={formData.secondName}
+                        handleChange={handleChange}
+                    />
+                </div>
+                <label className='label-title'>Bank Information</label>
+                <div className='form-type'>
+                    <FormInput
+                        label="card No."
+                        type="number"
+                        id="cardNo"
+                        name="cardNo"
+                        value={formData.cardNo}
+                        handleChange={handleChange}
+                    />
+                    <FormInput
+                        label="mpin"
+                        type="number"
+                        id="mpin"
+                        name="mpin"
+                        value={formData.mpin}
+                        handleChange={handleChange}
+                    />
+                </div>
                 <button type="submit" className="submit-btn" >register</button>
             </form>
-            
         </div>
     );
 }
